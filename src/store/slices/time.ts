2ec66e7081462f5778fa72b10dc44c005a9371a5
Denis Maliyshock <maliyshator@gmail.com>
2025-02-10 20:38:11 +0100

    move angle to the store

--- src/store/slices/time.ts
+++ src/store/slices/time.ts
@@ -1,27 +1,40 @@
 import { StateCreator } from "zustand";
 import { HOUR, MINUTE } from "~/constants.ts";
+import { calculateSunAngle } from "~/components/sun/utils/calculateSunAngle.ts";
+import { getMinutes } from "~/utils/getMinutes.ts";
+import { getHours } from "~/utils/getHours.ts";
 
 export type TimeSlice = {
   speed: number;
-  totalSeconds: number;
+  time: number;
   play: boolean;
   setPlay: (payload: boolean) => void;
   setTime: (payload: number) => void;
+  sunAngle: number;
 };
 
 const now = new Date();
 const hours = now.getHours();
 const minutes = now.getMinutes();
 const seconds = now.getSeconds();
+const initTime = hours * HOUR + minutes * MINUTE + seconds;
 
 const initialState = {
   speed: 1,
   play: true,
-  totalSeconds: hours * HOUR + minutes * MINUTE + seconds,
+  time: initTime,
+  sunAngle: calculateSunAngle(initTime),
 };
 
 export const timeSlice: StateCreator<TimeSlice, [], [], TimeSlice> = set => ({
   ...initialState,
   setPlay: (payload: boolean) => set({ play: payload }),
-  setTime: (payload: number) => set({ totalSeconds: payload }),
+  setTime: (payload: number) =>
+    set(store => {
+      const minutes = getMinutes(payload);
+      const hours = getHours(payload);
+      const shouldSunUpdate = getMinutes(store.time) !== minutes || getHours(store.time) !== hours;
+
+      return { time: payload, sunAngle: shouldSunUpdate ? calculateSunAngle(payload) : store.sunAngle };
+    }),
 });
