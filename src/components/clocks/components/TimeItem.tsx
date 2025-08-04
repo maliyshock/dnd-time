d8f4085811b3916a7043265b9c516c8cce3b1559
Denis Maliyshock <maliyshator@gmail.com>
2025-08-04 19:55:34 +0200

    add steps and steps-animation

--- src/components/clocks/components/TimeItem.tsx
+++ src/components/clocks/components/TimeItem.tsx
@@ -1,24 +1,40 @@
 import { formatTime } from "~/utils/time/formatTime.ts";
 import "../clocks.scss";
-import { memo } from "react";
+import { HTMLAttributes, memo } from "react";
 import { useGetPlaySample } from "~/hooks/useGetPlaySample.ts";
-import { Step } from "~/utils/generateSteps.ts";
 import { TimeChanger } from "~/components/clocks/components/TimeChanger.tsx";
+import useStore from "~/store/useStore.ts";
+import { HOUR_STEPS, MINUTE_STEPS, SECOND_STEPS } from "~/constants.ts";
+import { getSeconds } from "~/utils/time/getSeconds.ts";
+
+type TimeType = "hours" | "minutes" | "seconds";
 
 type TimeItemProps = {
-  value: number;
   fadeOut?: boolean;
-  steps: Step[];
   raiserVariation?: number;
   descenderVariation?: number;
   onTimeChange: (step: number) => void;
-};
+  timeType: TimeType;
+} & HTMLAttributes<HTMLDivElement>;
 
-export function TimeItem({ value, fadeOut = false, steps, onTimeChange }: TimeItemProps) {
+function getSteps(timeType: TimeType) {
+  switch (timeType) {
+    case "hours":
+      return HOUR_STEPS;
+    case "minutes":
+      return MINUTE_STEPS;
+    case "seconds":
+      return SECOND_STEPS;
+  }
+}
+
+export function TimeItem({ timeType, onTimeChange, ...props }: TimeItemProps) {
+  const timeValue = useStore(store => store[timeType === "seconds" ? "time" : timeType]);
   const playSample = useGetPlaySample({ name: "bip", shift: true });
+  const steps = getSteps(timeType);
 
   return (
-    <div className="clocks__item-wrapper">
+    <div {...props} className="clocks__item-wrapper rounded-4xl py-2">
       <TimeChanger
         steps={steps}
         onChange={stepValue => {
@@ -27,7 +43,9 @@ export function TimeItem({ value, fadeOut = false, steps, onTimeChange }: TimeIt
         }}
       />
 
-      <div className={`clocks__item  ${fadeOut ? "fade-out" : ""}`}>{formatTime(value)}</div>
+      <div className={`clocks__item  ${timeType === "seconds" ? "fade-out" : ""}`}>
+        {formatTime(timeType === "seconds" ? getSeconds(timeValue) : timeValue)}
+      </div>
 
       <TimeChanger
         steps={steps}
